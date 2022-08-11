47090871b1ab8a185cd86f66ecbec698440b76fa
Jennifer Byrnes <jennifer.byrnes@outlook.com>
2022-08-11 17:53:25 -0400

    initial commit - working input

--- index.js
+++ index.js
@@ -20,70 +20,62 @@ const firstQuestions = [
     },
   ]
 
-const nextQuestions = [
+const addADeptQuestions = [
     {
       type: 'input',
       name: 'newDeptName',
       message: 'What is the name of the department to be added?',
-      when: (promptTask === "Add a role")
     },
+  ]
 
+  const addARoleQuestions = [
     {
       type: 'input',
       name: 'newRoleName',
       message: 'What is the name of the role to be added?',
-      when: (promptTask === "Add a role")
     },
     {
       type: 'input',
       name: 'newRoleSalary',
       message: 'What is the salary for this role?',
-      when: (promptTask === "Add a role")
     },
     {
       type: 'input',
       name: 'newRoleDept',
       message: 'Into which department should this role be added?',
-      when: (promptTask === "Add a role")
     },
+  ]
+
+  const addAnEmpQuestions = [
     {
       type: 'input',
       name: 'newEmpFirstName',
       message: 'What is the first name of the employee to be added?',
-      when: (promptTask === "Add an employee")
     },
     {
       type: 'input',
       name: 'newEmpLastName',
       message: 'What is the last name of the employee to be added?',
-      when: (promptTask === "Add an employee")
     },
     {
       type: 'input',
       name: 'newEmpRole',
       message: "What is this employee's role?",
-      when: (promptTask === "Add an employee")
     },
     {
       type: 'input',
       name: 'newRoleDept',
       message: 'Into which department should this employee be added?',
-      when: (promptTask === "Add an employee")
     },
+  ]
+
+  const updtAnEmpQuestions = [
     {
       type: 'input',
       name: 'udptEmpRole',
       message: "What is this employee's new role?",
-      when: (promptTask === "Update an employee")
     },
-    {
-      type: 'input',
-      name: 'trash',
-      message: "Did this work?  ---" + promptTask + "---",
-    },
-]
-
- 
+  ]
 
 function ask() {
   inquirer.prompt(firstQuestions).then((answers) => {
@@ -102,11 +94,28 @@ function ask() {
       case 'View all employees':
         console.log("allEmps");
         break;
-      case 'View all roles':
-        console.log("allDepts");
+      case 'Add a department':
+        inquirer.prompt(addADeptQuestions).then((answers) => {
+          console.log("answers", answers);
+        })
+        break;
+      case 'Add a role':
+        inquirer.prompt(addARoleQuestions).then((answers) => {
+          console.log("answers", answers);
+        })
+        break;
+      case 'Add an employee':
+        inquirer.prompt(addAnEmpQuestions).then((answers) => {
+          console.log("answers", answers);
+        })
+        break;
+      case 'Update an employee role':
+        inquirer.prompt(updtAnEmpQuestions).then((answers) => {
+          console.log("answers", answers);
+        })
         break;
       default:
-        console.log("2nd round...", promptTask);
+        console.log("no selection for promptTask", promptTask);
         inquirer.prompt(nextQuestions).then((answers) => {
           console.log("answers", answers);
         })
